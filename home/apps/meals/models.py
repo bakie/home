09c2cb6aa47f0587123938223b978d97bfcdf96f
Bakie <bakelants.developer@gmail.com>
2020-02-02 13:42:38 +0100

    added ingredient + unit

--- home/apps/meals/models.py
+++ home/apps/meals/models.py
@@ -4,3 +4,23 @@ from django.db import models
 class Recipe(models.Model):
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
