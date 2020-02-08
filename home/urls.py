cad2dd3d9fa62bab76a876abb52069f2494dfca1
Bakie <bakelants.developer@gmail.com>
2020-02-08 09:40:45 +0100

    renamed base app to home app

--- home/urls.py
+++ home/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('meals/', include('home.apps.meals.urls')),
     path('admin/', admin.site.urls),
-    path('', include('home.apps.base.urls')),
+    path('', include('home.apps.home.urls')),
 ]
